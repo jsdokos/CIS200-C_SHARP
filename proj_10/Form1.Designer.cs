12f7578d487dd6e7a08a9028822d71d8f38592ae
jsdokos <jsdokos@gmail.com>
2015-05-05 01:12:44 -0500

    Project is done. Need to comment everything. A button to solve everyhting is done.

--- proj_10/Form1.Designer.cs
+++ proj_10/Form1.Designer.cs
@@ -28,12 +28,239 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.butt0_0 = new System.Windows.Forms.Button();
+            this.butt0_1 = new System.Windows.Forms.Button();
+            this.butt0_2 = new System.Windows.Forms.Button();
+            this.butt0_3 = new System.Windows.Forms.Button();
+            this.butt1_3 = new System.Windows.Forms.Button();
+            this.butt1_2 = new System.Windows.Forms.Button();
+            this.butt1_1 = new System.Windows.Forms.Button();
+            this.butt1_0 = new System.Windows.Forms.Button();
+            this.butt2_3 = new System.Windows.Forms.Button();
+            this.butt2_2 = new System.Windows.Forms.Button();
+            this.butt2_1 = new System.Windows.Forms.Button();
+            this.butt2_0 = new System.Windows.Forms.Button();
+            this.butt3_3 = new System.Windows.Forms.Button();
+            this.butt3_2 = new System.Windows.Forms.Button();
+            this.butt3_1 = new System.Windows.Forms.Button();
+            this.butt3_0 = new System.Windows.Forms.Button();
+            this.winnerBox = new System.Windows.Forms.RichTextBox();
+            this.autoWin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // butt0_0
+            // 
+            this.butt0_0.Location = new System.Drawing.Point(12, 12);
+            this.butt0_0.Name = "butt0_0";
+            this.butt0_0.Size = new System.Drawing.Size(72, 62);
+            this.butt0_0.TabIndex = 0;
+            this.butt0_0.Text = "butt0";
+            this.butt0_0.UseVisualStyleBackColor = true;
+            // 
+            // butt0_1
+            // 
+            this.butt0_1.Location = new System.Drawing.Point(90, 12);
+            this.butt0_1.Name = "butt0_1";
+            this.butt0_1.Size = new System.Drawing.Size(72, 62);
+            this.butt0_1.TabIndex = 1;
+            this.butt0_1.Text = "butt0";
+            this.butt0_1.UseVisualStyleBackColor = true;
+            // 
+            // butt0_2
+            // 
+            this.butt0_2.Location = new System.Drawing.Point(168, 12);
+            this.butt0_2.Name = "butt0_2";
+            this.butt0_2.Size = new System.Drawing.Size(72, 62);
+            this.butt0_2.TabIndex = 2;
+            this.butt0_2.Text = "butt0";
+            this.butt0_2.UseVisualStyleBackColor = true;
+            // 
+            // butt0_3
+            // 
+            this.butt0_3.Location = new System.Drawing.Point(246, 12);
+            this.butt0_3.Name = "butt0_3";
+            this.butt0_3.Size = new System.Drawing.Size(72, 62);
+            this.butt0_3.TabIndex = 3;
+            this.butt0_3.Text = "butt0";
+            this.butt0_3.UseVisualStyleBackColor = true;
+            // 
+            // butt1_3
+            // 
+            this.butt1_3.Location = new System.Drawing.Point(246, 80);
+            this.butt1_3.Name = "butt1_3";
+            this.butt1_3.Size = new System.Drawing.Size(72, 62);
+            this.butt1_3.TabIndex = 7;
+            this.butt1_3.Text = "butt0";
+            this.butt1_3.UseVisualStyleBackColor = true;
+            // 
+            // butt1_2
+            // 
+            this.butt1_2.Location = new System.Drawing.Point(168, 80);
+            this.butt1_2.Name = "butt1_2";
+            this.butt1_2.Size = new System.Drawing.Size(72, 62);
+            this.butt1_2.TabIndex = 6;
+            this.butt1_2.Text = "butt0";
+            this.butt1_2.UseVisualStyleBackColor = true;
+            // 
+            // butt1_1
+            // 
+            this.butt1_1.Location = new System.Drawing.Point(90, 80);
+            this.butt1_1.Name = "butt1_1";
+            this.butt1_1.Size = new System.Drawing.Size(72, 62);
+            this.butt1_1.TabIndex = 5;
+            this.butt1_1.Text = "butt0";
+            this.butt1_1.UseVisualStyleBackColor = true;
+            // 
+            // butt1_0
+            // 
+            this.butt1_0.Location = new System.Drawing.Point(12, 80);
+            this.butt1_0.Name = "butt1_0";
+            this.butt1_0.Size = new System.Drawing.Size(72, 62);
+            this.butt1_0.TabIndex = 4;
+            this.butt1_0.Text = "butt0";
+            this.butt1_0.UseVisualStyleBackColor = true;
+            // 
+            // butt2_3
+            // 
+            this.butt2_3.Location = new System.Drawing.Point(246, 148);
+            this.butt2_3.Name = "butt2_3";
+            this.butt2_3.Size = new System.Drawing.Size(72, 62);
+            this.butt2_3.TabIndex = 11;
+            this.butt2_3.Text = "butt0";
+            this.butt2_3.UseVisualStyleBackColor = true;
+            // 
+            // butt2_2
+            // 
+            this.butt2_2.Location = new System.Drawing.Point(168, 148);
+            this.butt2_2.Name = "butt2_2";
+            this.butt2_2.Size = new System.Drawing.Size(72, 62);
+            this.butt2_2.TabIndex = 10;
+            this.butt2_2.Text = "butt0";
+            this.butt2_2.UseVisualStyleBackColor = true;
+            // 
+            // butt2_1
+            // 
+            this.butt2_1.Location = new System.Drawing.Point(90, 148);
+            this.butt2_1.Name = "butt2_1";
+            this.butt2_1.Size = new System.Drawing.Size(72, 62);
+            this.butt2_1.TabIndex = 9;
+            this.butt2_1.Text = "butt0";
+            this.butt2_1.UseVisualStyleBackColor = true;
+            // 
+            // butt2_0
+            // 
+            this.butt2_0.Location = new System.Drawing.Point(12, 148);
+            this.butt2_0.Name = "butt2_0";
+            this.butt2_0.Size = new System.Drawing.Size(72, 62);
+            this.butt2_0.TabIndex = 8;
+            this.butt2_0.Text = "butt0";
+            this.butt2_0.UseVisualStyleBackColor = true;
+            // 
+            // butt3_3
+            // 
+            this.butt3_3.Location = new System.Drawing.Point(246, 216);
+            this.butt3_3.Name = "butt3_3";
+            this.butt3_3.Size = new System.Drawing.Size(72, 62);
+            this.butt3_3.TabIndex = 15;
+            this.butt3_3.Text = "butt0";
+            this.butt3_3.UseVisualStyleBackColor = true;
+            // 
+            // butt3_2
+            // 
+            this.butt3_2.Location = new System.Drawing.Point(168, 216);
+            this.butt3_2.Name = "butt3_2";
+            this.butt3_2.Size = new System.Drawing.Size(72, 62);
+            this.butt3_2.TabIndex = 14;
+            this.butt3_2.Text = "butt0";
+            this.butt3_2.UseVisualStyleBackColor = true;
+            // 
+            // butt3_1
+            // 
+            this.butt3_1.Location = new System.Drawing.Point(90, 216);
+            this.butt3_1.Name = "butt3_1";
+            this.butt3_1.Size = new System.Drawing.Size(72, 62);
+            this.butt3_1.TabIndex = 13;
+            this.butt3_1.Text = "butt0";
+            this.butt3_1.UseVisualStyleBackColor = true;
+            // 
+            // butt3_0
+            // 
+            this.butt3_0.Location = new System.Drawing.Point(12, 216);
+            this.butt3_0.Name = "butt3_0";
+            this.butt3_0.Size = new System.Drawing.Size(72, 62);
+            this.butt3_0.TabIndex = 12;
+            this.butt3_0.Text = "butt0";
+            this.butt3_0.UseVisualStyleBackColor = true;
+            // 
+            // winnerBox
+            // 
+            this.winnerBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winnerBox.ForeColor = System.Drawing.Color.Orange;
+            this.winnerBox.Location = new System.Drawing.Point(12, 284);
+            this.winnerBox.Name = "winnerBox";
+            this.winnerBox.Size = new System.Drawing.Size(280, 96);
+            this.winnerBox.TabIndex = 17;
+            this.winnerBox.Text = "You Win!!!!";
+            // 
+            // autoWin
+            // 
+            this.autoWin.Location = new System.Drawing.Point(-4, 370);
+            this.autoWin.Name = "autoWin";
+            this.autoWin.Size = new System.Drawing.Size(10, 10);
+            this.autoWin.TabIndex = 18;
+            this.autoWin.Text = "Win";
+            this.autoWin.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(380, 382);
+            this.Controls.Add(this.autoWin);
+            this.Controls.Add(this.winnerBox);
+            this.Controls.Add(this.butt3_3);
+            this.Controls.Add(this.butt3_2);
+            this.Controls.Add(this.butt3_1);
+            this.Controls.Add(this.butt3_0);
+            this.Controls.Add(this.butt2_3);
+            this.Controls.Add(this.butt2_2);
+            this.Controls.Add(this.butt2_1);
+            this.Controls.Add(this.butt2_0);
+            this.Controls.Add(this.butt1_3);
+            this.Controls.Add(this.butt1_2);
+            this.Controls.Add(this.butt1_1);
+            this.Controls.Add(this.butt1_0);
+            this.Controls.Add(this.butt0_3);
+            this.Controls.Add(this.butt0_2);
+            this.Controls.Add(this.butt0_1);
+            this.Controls.Add(this.butt0_0);
+            this.Name = "Form1";
+            this.Text = "Slide Puzzle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button butt0_0;
+        private System.Windows.Forms.Button butt0_1;
+        private System.Windows.Forms.Button butt0_2;
+        private System.Windows.Forms.Button butt0_3;
+        private System.Windows.Forms.Button butt1_3;
+        private System.Windows.Forms.Button butt1_2;
+        private System.Windows.Forms.Button butt1_1;
+        private System.Windows.Forms.Button butt1_0;
+        private System.Windows.Forms.Button butt2_3;
+        private System.Windows.Forms.Button butt2_2;
+        private System.Windows.Forms.Button butt2_1;
+        private System.Windows.Forms.Button butt2_0;
+        private System.Windows.Forms.Button butt3_3;
+        private System.Windows.Forms.Button butt3_2;
+        private System.Windows.Forms.Button butt3_1;
+        private System.Windows.Forms.Button butt3_0;
+        private System.Windows.Forms.RichTextBox winnerBox;
+        private System.Windows.Forms.Button autoWin;
     }
 }
 
--- proj_10/Form1.cs
+++ proj_10/Form1.cs
@@ -12,9 +12,168 @@ namespace proj_10
 {
     public partial class Form1 : Form
     {
+
+        Button[,] butt_array;
+
+        //constructor
         public Form1()
         {
             InitializeComponent();
+
+            butt_array = new Button[4, 4] { 
+                              {butt0_0, butt0_1, butt0_2, butt0_3},
+                              {butt1_0, butt1_1, butt1_2, butt1_3},
+                              {butt2_0, butt2_1, butt2_2, butt2_3},
+                              {butt3_0, butt3_1, butt3_2, butt3_3}
+                              };
+            randomButton();
+            winnerBox.Visible = false;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    butt_array[i, j].Click += new EventHandler(shiftClick);
+                }
+            }
+            autoWin.Click += new EventHandler(winitAll);
+        }
+
+        private void winitAll(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            int num = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    butt_array[i, j].Text = num++.ToString();
+                    butt_array[i, j].Visible = true;
+                }
+            }
+            butt_array[3, 2].Text = "16";
+            butt_array[3, 2].Visible = false;
+
+            butt_array[3, 3].Text = "15";
+            butt_array[3, 3].Visible = true;
+        }
+
+        private void randomButton()
+        {
+            Random r = new Random();
+            int num = 0;
+            bool foundNum = false;
+
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    foundNum = false;
+                    do
+                    {
+                        num = r.Next(16);
+                        if (list.Contains(num + 1) != true)
+                        {
+                            butt_array[i, j].Text = (num + 1).ToString();
+                            list.Add(num + 1);
+                            foundNum = true;
+                            if (num == 15) // since the random num is one less
+                                butt_array[i, j].Visible = false;
+
+                        }
+                    }
+                    while (!foundNum);
+                }
+            }
+        }
+
+        private void shiftClick(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            int butt_row = 0;
+            int butt_col = 0;
+            int blank_row = 0;
+            int blank_col = 0;
+            
+
+            //find the col and row of the clicked button
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    //find array position for the blank button
+                    if (butt_array[i,j].Text == "16")
+                    {
+                        blank_row = i;
+                        blank_col = j;
+                    }
+
+                    //find array position for the one that was pressed
+                    if (butt_array[i,j].Text == b.Text)
+                    {
+                        butt_row = i;
+                        butt_col = j;
+                    }
+                }
+            }
+
+            if (butt_col == blank_col && butt_row != blank_row && Math.Abs(butt_row - blank_row) == 1)
+            {
+                butt_array[blank_row, blank_col].Text = butt_array[butt_row, butt_col].Text; //changing the button text
+                butt_array[butt_row, butt_col].Text = "16"; // making the old button the new blank button
+
+                //flipping the visible button
+                butt_array[butt_row, butt_col].Visible = false;
+                butt_array[blank_row, blank_col].Visible = true;
+            }
+            else if (butt_col != blank_col && butt_row == blank_row && Math.Abs(butt_col - blank_col) == 1)
+            {
+                butt_array[blank_row, blank_col].Text = butt_array[butt_row, butt_col].Text; //changing the button text
+                butt_array[butt_row, butt_col].Text = "16"; // making the old button the new blank button
+
+                //flipping the visible button
+                butt_array[butt_row, butt_col].Visible = false;
+                butt_array[blank_row, blank_col].Visible = true;
+            }  
+
+            if (isWinner() == true)
+            {
+                winnerBox.Visible = true;
+
+                //disable all buttons
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        butt_array[i, j].Enabled = false;
+                    }
+                }
+            }
+        }
+
+        private bool isWinner()
+        {
+            int num = 1;
+            bool isWinner = true;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (butt_array[i, j].Text == num++.ToString())
+                        isWinner = true;
+                    else
+                    {
+                        isWinner = false;
+                        return isWinner;
+                    }
+                }
+            }
+            return isWinner;
+        }
+ 
+        private void Form1_Load(object sender, EventArgs e)
+        {
         }
     }
 }
